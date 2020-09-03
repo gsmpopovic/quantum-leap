260683c909b832e7bfa2f0435cd9abb29e072f1d
George Popovic <georgesmpopovic@gmail.com>
2020-09-03 13:59:22 -0400

    More styling. Tweaking.

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,6 +1,6 @@
 <header>
-        <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid p-0">
+            <nav class="navbar navbar-expand-lg navbar-darktext-light">
                 <a class="navbar-brand" href="index.php">Quantum Leap</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
